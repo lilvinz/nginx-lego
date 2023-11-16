616b2346f3038f6c9dd2a5d8f272590a3bc465f8
lilvinz <vincent.kessler@quantec-group.com>
2023-11-16 12:38:15 +0100

    add 3rd domain

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,8 +32,23 @@ if [ ! -f "/var/lego/certificates/${DOMAIN2}.crt" ] || [ ! -f "/var/lego/certifi
     RESULT=$?
 fi
 
+# If the cert/key don't exist...
+if [ ! -f "/var/lego/certificates/${DOMAIN3}.crt" ] || [ ! -f "/var/lego/certificates/${DOMAIN3}.key" ]; then
+    # issue the certificate
+    lego \
+        --path="/var/lego" \
+        --server="${LEGO_SERVER:=https://acme-v02.api.letsencrypt.org/directory}" \
+        --email="${EMAIL}" \
+        --domains=${DOMAIN3} \
+        --dns="${DNS_PROVIDER}" \
+        --dns.resolvers="${DNS_RESOLVER:="8.8.8.8"}" \
+        -a \
+        run
+    RESULT=$?
+fi
+
 if [ $RESULT -eq 0 ]; then
-  envsubst '${UPSTREAM_SERVER} ${DOMAIN} ${UPSTREAM_SERVER2} ${DOMAIN2}' < /config/nginx.conf > /etc/nginx/nginx.conf
+  envsubst '${UPSTREAM_SERVER} ${DOMAIN} ${UPSTREAM_SERVER2} ${DOMAIN2} ${UPSTREAM_SERVER3} ${DOMAIN3}' < /config/nginx.conf > /etc/nginx/nginx.conf
 
   supervisord -c /supervisord.conf
 fi
