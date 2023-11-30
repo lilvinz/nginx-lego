324090644005d9c927481b7ff72871ef82628a39
lilvinz <vincent.kessler@quantec-group.com>
2023-11-30 12:45:56 +0100

    extend to 4 domains

--- entrypoint.sh
+++ entrypoint.sh
@@ -47,8 +47,23 @@ if [ ! -f "/var/lego/certificates/${DOMAIN3}.crt" ] || [ ! -f "/var/lego/certifi
     RESULT=$?
 fi
 
+# If the cert/key don't exist...
+if [ ! -f "/var/lego/certificates/${DOMAIN4}.crt" ] || [ ! -f "/var/lego/certificates/${DOMAIN4}.key" ]; then
+    # issue the certificate
+    lego \
+        --path="/var/lego" \
+        --server="${LEGO_SERVER:=https://acme-v02.api.letsencrypt.org/directory}" \
+        --email="${EMAIL}" \
+        --domains=${DOMAIN4} \
+        --dns="${DNS_PROVIDER}" \
+        --dns.resolvers="${DNS_RESOLVER:="8.8.8.8"}" \
+        -a \
+        run
+    RESULT=$?
+fi
+
 if [ $RESULT -eq 0 ]; then
-  envsubst '${UPSTREAM_SERVER} ${DOMAIN} ${UPSTREAM_SERVER2} ${DOMAIN2} ${UPSTREAM_SERVER3} ${DOMAIN3}' < /config/nginx.conf > /etc/nginx/nginx.conf
+  envsubst '${UPSTREAM_SERVER} ${DOMAIN} ${UPSTREAM_SERVER2} ${DOMAIN2} ${UPSTREAM_SERVER3} ${DOMAIN3} ${UPSTREAM_SERVER4} ${DOMAIN4}' < /config/nginx.conf > /etc/nginx/nginx.conf
 
   supervisord -c /supervisord.conf
 fi
--- renew.sh
+++ renew.sh
@@ -37,6 +37,16 @@ lego \
     renew \
     --days="${RENEW_DAYS:="30"}"
 
+lego \
+    --path="/var/lego" \
+    --server="${LEGO_SERVER:=https://acme-v02.api.letsencrypt.org/directory}" \
+    --email="${EMAIL}" \
+    --domains=${DOMAIN4} \
+    --dns="${DNS_PROVIDER}" \
+    --dns.resolvers="${DNS_RESOLVER:="8.8.8.8"}" \
+    renew \
+    --days="${RENEW_DAYS:="30"}"
+
 if [ "${PRE_HASH}" = "$(hash_certs)" ]; then
     echo "Certificate is still valid for at least ${RENEW_DAYS} days."
     exit 0
